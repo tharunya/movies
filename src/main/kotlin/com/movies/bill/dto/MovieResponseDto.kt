32a53be2155f80b34a1f1328c148cde97d771e06
Tharunya Pati <tharunyapati@gmail.com>
2023-04-13 01:30:22 -0700

    Movies: create movie and get with actors

---
+++ src/main/kotlin/com/movies/bill/dto/MovieResponseDto.kt
@@ -0,0 +1,9 @@
+package com.movies.bill.dto
+
+data class MovieResponseDto (
+    var movieid: String,
+    var movietitle: String,
+    var moviereleaseDate: String,
+    var actorId: String,
+    var actorName: String,
+)
